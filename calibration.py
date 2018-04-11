ce1c3905e10dc54d22092d82d2f11f95900b6a61
Roman Pilyugin <roman.pilyugin@gmail.com>
2018-04-11 18:36:05 +0300

    Add again calibration class file

---
+++ calibration.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+
+Point = namedtuple('Point', 'x y')
+Screen_size = namedtuple('Screen_size', 'width height')
+
+calibration_data = {}
+calibration_data['TLEyePos'] = Point(0, 0)
+calibration_data['TREyePos'] = Point(0, 0)
+calibration_data['BLEyePos'] = Point(0, 0)
+calibration_data['BREyePos'] = Point(0, 0)
+
+
+class Calibrator():
+    def __init__(self, screen_size, cal_data=None):
+        self.screen_size = Screen_size(*screen_size)
+        if cal_data is not None:
+            self.setCalData(cal_data)
+        pass
+
+    def setCalData(self, cal_data):
+        self.xeyeleft = (cal_data['TLEyePos'].x + cal_data['BLEyePos'].x) / 2;
+        self.xyeyright = (cal_data['TREyePos'].x + cal_data['BREyePos'].x) / 2;
+        self.yeyetop = (cal_data['TLEyePos'].y + cal_data['TREyePos'].y) / 2;
+        self.yeyebottom = (cal_data['BLEyePos'].y + cal_data['BREyePos'].y) / 2;
+        pass
+
+    def translate(self, eyecenter):
+        if eyecenter:
+            EyeCenter = Point(*eyecenter)
+            x = self.screen_size.width * (EyeCenter.x - self.xeyeleft) / (self.xyeyright - self.xeyeleft)
+            y = self.screen_size.height * (EyeCenter.y - self.yeyetop) / (self.yeyebottom - self.yeyetop);
+            return (x, y)
