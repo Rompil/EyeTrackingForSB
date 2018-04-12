66576c06723b3c91906e46e99c7191daf40fd5c2
Roman Pilyugin <roman.pilyugin@gmail.com>
2018-04-12 17:11:55 +0300

    Added test calibration

--- calibration.py
+++ calibration.py
@@ -4,10 +4,10 @@ Point = namedtuple('Point', 'x y')
 Screen_size = namedtuple('Screen_size', 'width height')
 
 calibration_data = {}
-calibration_data['TLEyePos'] = Point(0, 0)
-calibration_data['TREyePos'] = Point(0, 0)
-calibration_data['BLEyePos'] = Point(0, 0)
-calibration_data['BREyePos'] = Point(0, 0)
+calibration_data['TLEyePos'] = Point(10, 10)
+calibration_data['TREyePos'] = Point(10, 10)
+calibration_data['BLEyePos'] = Point(-10, -10)
+calibration_data['BREyePos'] = Point(10, -10)
 
 
 class Calibrator():
@@ -18,15 +18,21 @@ class Calibrator():
         pass
 
     def setCalData(self, cal_data):
-        self.xeyeleft = (cal_data['TLEyePos'].x + cal_data['BLEyePos'].x) / 2;
-        self.xyeyright = (cal_data['TREyePos'].x + cal_data['BREyePos'].x) / 2;
-        self.yeyetop = (cal_data['TLEyePos'].y + cal_data['TREyePos'].y) / 2;
-        self.yeyebottom = (cal_data['BLEyePos'].y + cal_data['BREyePos'].y) / 2;
+        self.xeyeleft = (cal_data['TLEyePos'].x + cal_data['BLEyePos'].x) / 2
+        self.xyeyright = (cal_data['TREyePos'].x + cal_data['BREyePos'].x) / 2
+        self.yeyetop = (cal_data['TLEyePos'].y + cal_data['TREyePos'].y) / 2
+        self.yeyebottom = (cal_data['BLEyePos'].y + cal_data['BREyePos'].y) / 2
         pass
 
     def translate(self, eyecenter):
+        # print('translate func {}'.format(eyecenter))
         if eyecenter:
             EyeCenter = Point(*eyecenter)
             x = self.screen_size.width * (EyeCenter.x - self.xeyeleft) / (self.xyeyright - self.xeyeleft)
-            y = self.screen_size.height * (EyeCenter.y - self.yeyetop) / (self.yeyebottom - self.yeyetop);
+            y = self.screen_size.height * (EyeCenter.y - self.yeyetop) / (self.yeyebottom - self.yeyetop)
             return (x, y)
+
+
+if __name__ == '__main__':
+    c = Calibrator((100, 100), calibration_data)
+    print(c.translate((0, 0)))
